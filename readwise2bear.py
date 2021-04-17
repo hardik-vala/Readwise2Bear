88ae5241c8f8d89b52566a37b920d84113b273e2
Hardik Vala <hardikvala24@gmail.com>
2021-04-16 21:37:40 -0700

    Create script to print book id, title pairs

---
+++ readwise2bear.py
@@ -0,0 +1,33 @@
+import argparse
+import requests
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--readwise_token", help="Readwise API access token")
+
+def main():
+	args = parser.parse_args()
+
+	books_data = get_books_data(args.readwise_token)
+	book_ids_and_titles = extract_book_ids_and_titles(books_data)
+
+	for b in book_ids_and_titles:
+		print("id: %s, title: %s" % b)
+
+def get_books_data(readwise_token):
+	querystring = {
+	    "category": "books",
+	}
+
+	response = requests.get(
+	    url="https://readwise.io/api/v2/books/",
+	    headers={"Authorization": readwise_token},
+	    params=querystring
+	)
+
+	return response.json()
+
+def extract_book_ids_and_titles(books_data):
+	return [(b["id"], b["title"]) for b in books_data["results"]]
+
+if __name__ == "__main__":
+	main()
