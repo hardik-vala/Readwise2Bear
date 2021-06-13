b83c7e78caf925bf9bca0452d3d3d861a0670b41
Hardik Vala <hardikvala24@gmail.com>
2021-06-12 21:37:11 -0700

    Add --print_books and book highlights fetching

--- readwise2bear.py
+++ readwise2bear.py
@@ -1,8 +1,13 @@
 import argparse
 import requests
 
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--readwise_token", help="Readwise API access token (in the form \"Token XXX\")")
+parser.add_argument("--readwise_token",
+	help="Readwise API access token (in the form \"Token XXX\")")
+parser.add_argument("--print_books", dest='print_books', action='store_true',
+	help="Print the book Ids and titles to the console")
+
 
 def main():
 	args = parser.parse_args()
@@ -10,24 +15,40 @@ def main():
 	books_data = get_books_data(args.readwise_token)
 	book_ids_and_titles = extract_book_ids_and_titles(books_data)
 
-	for b in book_ids_and_titles:
-		print("id: %s, title: %s" % b)
+	if args.print_books:
+		for book_id, book_title in book_ids_and_titles:
+			print("id: {}, title: {}".format(book_id, book_title))
+
 
 def get_books_data(readwise_token):
-	querystring = {
-	    "category": "books",
+	query_string = {
+		"category": "books",
 	}
 
-	response = requests.get(
-	    url="https://readwise.io/api/v2/books/",
-	    headers={"Authorization": readwise_token},
-	    params=querystring
-	)
+	return send_readwise_request("books/", readwise_token, query_string)
 
-	return response.json()
 
 def extract_book_ids_and_titles(books_data):
 	return [(b["id"], b["title"]) for b in books_data["results"]]
+
+
+def get_highlights(readwise_token, book_id):
+	query_string = {
+		"page_size": 3,
+		"book_id": book_id,
+	}
+
+	return send_readwise_request("highlights/", readwise_token, query_string)
+
+
+def send_readwise_request(path_suffix, readwise_token, params):
+	response = requests.get(
+		url="https://readwise.io/api/v2/{}".format(path_suffix),
+		headers={"Authorization": readwise_token},
+		params=params
+	)
+	return response.json()
+
 
 if __name__ == "__main__":
 	main()
