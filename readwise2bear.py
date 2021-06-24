b61d6a3b11499e46276b64c6b61e1fb2502b6724
Hardik Vala <hardikvala24@gmail.com>
2021-06-23 22:07:22 -0700

    Add from date filtering on highlights and export highlight to Bear

--- readwise2bear.py
+++ readwise2bear.py
@@ -1,10 +1,16 @@
 import argparse
+import datetime
+import json
 import requests
+import webbrowser
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--readwise_token",
 	help="Readwise API access token (in the form \"Token XXX\")")
+parser.add_argument("--from_date", dest='from_date',
+	type=lambda s: datetime.datetime.strptime(s, "%Y-%m-%d"),
+	help="Date after which (inclusive) to filter highlights")
 parser.add_argument("--print_books", dest='print_books', action='store_true',
 	help="Print the book Ids and titles to the console")
 
@@ -15,10 +21,18 @@ def main():
 	books_data = get_books_data(args.readwise_token)
 	book_ids_and_titles = extract_book_ids_and_titles(books_data)
 
-	if args.print_books:
-		for book_id, book_title in book_ids_and_titles:
+	for book_id, book_title in book_ids_and_titles:
+		if args.print_books:
 			print("id: {}, title: {}".format(book_id, book_title))
-
+		else:
+			highlights = get_highlights(args.readwise_token, book_id)
+			for highlight in highlights:
+				highlight_date = datetime.datetime.strptime(highlight["highlighted_at"], "%Y-%m-%dT%H:%M:%SZ")
+				if highlight_date >= args.from_date:
+					webbrowser.open("bear://x-callback-url/create?text=%s" % highlight)
+					break
+		break
+			
 
 def get_books_data(readwise_token):
 	query_string = {
@@ -38,7 +52,10 @@ def get_highlights(readwise_token, book_id):
 		"book_id": book_id,
 	}
 
-	return send_readwise_request("highlights/", readwise_token, query_string)
+	response = send_readwise_request("highlights/", readwise_token,
+		query_string)
+	
+	return response["results"]
 
 
 def send_readwise_request(path_suffix, readwise_token, params):
